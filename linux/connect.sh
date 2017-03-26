b79c05b63edaa4b876e4fa452f1c35a714147f39
Anooyman <875734078@qq.com>
2017-03-26 10:52:59 +0800

    rewrite linux/connect.sh

--- linux/connect.sh
+++ linux/connect.sh
@@ -3,5 +3,4 @@
 #Created Time:2016-05-07 22:11:57
 #########################################################################
 #!/bin/bash
-setxkbmap -option "ctrl:swapcaps"
 mentohust;kill -9 $a={ps aux | grep mentohust | grep -v grep | awk '{print $2}'};mentohust -b1
